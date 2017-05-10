d7782bed4d11f4efd5dc52a26e5892639b2d5f0e
Nguyen Van Tai <nguyenvantai20121992@gmail.com>
2017-05-10 22:09:12 +0700

    laravel version 1

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Trang Chủ</title>
+	<meta charset="utf-8">
+	<link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+	 <script src="{{asset('js/jquery-3.2.1.min.js')}}"></script>
+	<script src="{{asset('js/bootstrap.min.js')}}"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-2">
+				<p>col-md-2</p>
+			</div>
+			<div class="col-md-8">
+				<p>col-md-8</p>
+			</div>
+			<div class="col-md-2">
+				<p>col-md-2</p>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
